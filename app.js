088b08aa16687f17a4a59175fa2cc2f8822307a8
Mufaro1410 <fungurayimufaro@gmail.com>
2023-12-13 09:44:07 +0200

    configure github actions

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-import express from 'express'
-import cors from 'cors'
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
 
 const app = express()
 
-app.use(cors)
+app.use(cors())
+app.use(express.static(path.join(__dirname, '/frontend/build')))
 
 app.get('/name', (req, res) => {
     res.send('Mufaro')
